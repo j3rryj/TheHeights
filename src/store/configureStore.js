ce2724d183c41223d307f7cb2ca3f0b10a3f4f76
Jerry Jong <jerryjong01@gmail.com>
2017-11-06 13:07:22 -0800

    initialize redux store

---
+++ src/store/configureStore.js
@@ -0,0 +1,11 @@
+import { createStore, applyMiddleware } from 'redux';
+import ReduxPromise from 'redux-promise';
+
+import rootReducer from '../reducers/index';
+
+export default function configureStore() {
+  return createStore(
+    rootReducer,
+    applyMiddleware(ReduxPromise),
+  );
+}
