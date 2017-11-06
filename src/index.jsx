7efb5537b57c627160aac4bcd42736429d6441ea
Jerry Jong <jerryjong01@gmail.com>
2017-11-06 13:13:18 -0800

    entry point for React app'

---
+++ src/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import App from './components/App.jsx';
+import configureStore from './store/configureStore';
+
+const store = configureStore();
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>
+  , document.querySelector('.root')
+);
