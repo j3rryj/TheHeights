6cc795a29546a1c6ae406a2ee6b0d1e2a921b5a1
Jerry Jong <jerryjong01@gmail.com>
2017-11-06 16:02:40 -0800

    added header

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Header from '../common/Header.jsx';
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
+
+export default App;
